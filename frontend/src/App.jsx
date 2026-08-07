b66871fa90a30fe7c13339e5bbce11e5e3d389e7
Sirivally0108 <singarisirivally987@gmail.com>
2026-08-07 16:57:46 +0530

    Initial project setup with React, Express and routing

---
+++ frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Jobs from "./pages/Jobs";
+import JobDetails from "./pages/JobDetails";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import EmployerDashboard from "./pages/EmployerDashboard";
+import CandidateDashboard from "./pages/CandidateDashboard";
+import NotFound from "./pages/NotFound";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/jobs" element={<Jobs />} />
+        <Route path="/jobs/:id" element={<JobDetails />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/employer" element={<EmployerDashboard />} />
+        <Route path="/candidate" element={<CandidateDashboard />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
