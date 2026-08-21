fc148d1f205a6ef5b155856fda6c397b5a877b5d
Sirivally0108 <singarisirivally987@gmail.com>
2026-08-21 22:18:07 +0530

    Protect users route with authentication

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,10 +6,14 @@ const {
   getUsers,
 } = require("../controllers/userController");
 
+const authMiddleware = require("../middleware/authMiddleware");
+
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.get("/", getUsers);
+
+// Protected users route
+router.get("/", authMiddleware, getUsers);
 
 module.exports = router;
